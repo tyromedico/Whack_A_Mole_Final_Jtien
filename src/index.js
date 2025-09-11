05af4ed200a4c766cbfedec527d1f36365ed92ac
towershield <kekatar@gmail.com>
2025-09-11 16:02:05 -0700

    set random integer, difficulty

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -41,7 +41,13 @@ function randomInteger(min, max) {
  */
 function setDelay(difficulty) {
   // TODO: Write your code here.
-  
+  if (difficulty === "easy") {
+    return 1500;
+  } else if (difficulty === "normal") {
+    return 1200;
+  } else if (difficulty === "hard") {
+    return (Math.floor(Math.random()*1200)+600);
+  }
 }
 
 /**
